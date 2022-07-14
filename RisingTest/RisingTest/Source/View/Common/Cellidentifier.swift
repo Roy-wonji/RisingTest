5aa4470fbd8c2ad1e06c5607e445e032008d7467
Roy-wonji <shuwj81@daum.net>
2022-07-14 22:42:30 +0900

    🪛[chore]: CellIdentifier 기본 설정 및 구현

---
+++ RisingTest/RisingTest/Source/View/Common/Cellidentifier.swift
@@ -0,0 +1,13 @@
+//
+//  Cellidentifier.swift
+//  RisingTest
+//
+//  Created by 서원지 on 2022/07/14.
+//
+
+import Foundation
+
+//MARK: - 셀의 Identifier
+struct Cellidentifier {
+    static let baseIdentifier = "\(Cellidentifier.self)"
+}
